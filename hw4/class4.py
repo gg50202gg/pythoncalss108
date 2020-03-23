a7c3bbd3b12f1300456b967668195a84614ae128
Zark Huang <smile0986875211@gmail.com>
2020-03-23 17:39:10 +0800

    class finished

---
+++ hw4/class4.py
@@ -0,0 +1,29 @@
+import random
+
+again = 'Y'
+dict_win = [None, 3, 1, 2]
+dict_go = {
+    1: "剪刀",
+    2: "石頭",
+    3: "布",
+}
+
+
+def main():
+    player = eval(input("請問你出什麼?\n1.剪刀\n2.石頭\n3.布\n... "))
+    computer = random.randint(1, 3)
+    print("隨機數: %d" % (computer))
+    if player == computer:  # 平手
+        go = dict_go[player]
+        print("你出 %s ,電腦也出 %s ,平手" % (go, go))
+    elif dict_win[computer] == player:  # 電腦win
+        print("你出 %s ,電腦出 %s ,電腦贏" % (dict_go[player], dict_go[computer]))
+    else:  # 玩家win
+        print("你出 %s ,電腦出 %s ,玩家贏" % (dict_go[player], dict_go[computer]))
+
+
+if __name__ == "__main__":
+    while again == "Y" or again == 'y':
+        main()
+        again = input("Continue?(Y/N)")
+    print("結束遊戲")
---
+++ hw4/hw4.py
@@ -0,0 +1,19 @@
+import datetime as dt
+
+
+def main():
+    year = input("Which year are you born? ")
+    month = input("Which month?  1-12 ")
+    born = dt.datetime.strptime(year+"-"+month, "%Y-%m")
+    print("you are born on %s, %s" %
+          (born.strftime("%Y"), born.strftime("%b")))
+    print("you are %s years old." % (
+        round((dt.datetime.today()-born).days/365)
+    ))
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        if "Y" == input("繼續?(Y/N)"):
+            break
